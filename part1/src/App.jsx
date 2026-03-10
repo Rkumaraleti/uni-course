f40cd89541e6ed8bf74f7e59c81ada4508e638db
Renuka Kumar Aleti <kumar@spotmies.ai>
2026-03-10 23:39:28 +0530

    Part 1: 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
-function App() {
-  const [count, setCount] = useState(0)
-  console.log("Hello from component")
+  const Header = (props) => {
+    return (
+      <h1>
+        {props.course}
+      </h1>
+    )
+  }
+
+  const Content = (props) => {
+    const { items } = props;
+    return (
+      items.map(item => {
+        return (
+          <p>
+            {item.part} {item.exercises}
+          </p>
+        )
+      })
+    )
+  }
+
+  const Total = (props) => {
+    const { value } = props;
+    return (
+      <p>Number of exercises {value}</p>
+    )
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <Header course={course} />
+      <Content items={[{ part: part1, exercises: exercises1 }, { part: part2, exercises: exercises2 }, { part: part3, exercises: exercises3 }]} />
+      <Total value={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
